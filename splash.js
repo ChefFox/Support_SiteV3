ff223ca2cbcc02a6df6e1830704e84ffbcf377f4
ChefFox <hello@dot9ideas.com>
2025-06-11 17:45:10 +0100

    Add files via upload

--- splash.js
+++ splash.js
@@ -1,14 +1,19 @@
 
 function checkPassword() {
     const overlay = document.getElementById("overlay");
+    const fade = document.getElementById("fade");
     const input = document.getElementById("password").value.trim().toLowerCase();
 
     if (input === "mellon") {
         overlay.classList.remove("visible");
         overlay.classList.add("hidden");
         setTimeout(() => {
-            window.location.href = "real.html";
-        }, 1500);
+            fade.classList.remove("hidden");
+            fade.classList.add("visible");
+            setTimeout(() => {
+                window.location.href = "real.html";
+            }, 1500);
+        }, 1200);
     } else {
         overlay.classList.remove("visible");
         overlay.classList.add("hidden");
